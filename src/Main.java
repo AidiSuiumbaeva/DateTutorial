99cf94b9bc5eb59fa54fee440b761444b95d0e50
Perchik <saikalrustamovna@gmail.com>
2023-08-15 19:15:23 +0600

    smth is added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("we didn't add Date ");
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello world! what's your name, dude? ");
+        String userName = scanner.nextLine();
+        System.out.println("we didn't add Date, " + userName);
     }
 }
